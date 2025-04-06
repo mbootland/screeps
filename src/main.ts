c323fea02fb16c77869ce411fdddc3530f8438c4
unknown <matthew.bootland@gmail.com>
2025-04-06 18:41:06 +0900

    Updating locations

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import roleHarvester from './role.harvester';
-
-export function loop(): void {
-  for(const name in Game.creeps) {
-    const creep = Game.creeps[name];
-    roleHarvester.run(creep);
-  }
-}
